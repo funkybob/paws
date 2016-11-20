ed9294c7ab0abf574f076464274d83f1e39b53cd
Curtis Maloney <curtis@tinbrain.net>
2016-11-21 08:16:03 +1100

    Handle and log exceptions.  Render Response objects.

--- paws/handler.py
+++ paws/handler.py
@@ -1,5 +1,8 @@
 from .request import Request
-from .response import response
+from .response import response, Response
+
+import logging
+log = logging.getLogger()
 
 
 class Handler(object):
@@ -10,8 +13,25 @@ class Handler(object):
         self.request = Request(event, context)
 
     def __call__(self, event, context):
-        func = getattr(self, self.event['httpMethod'], self.invalid)
-        return func(self.request, *self.event['pathParameters'])
+        func = getattr(self, self.event['httpMethod'].lower(), self.invalid)
+        try:
+            resp = func(self.request, *self.event['pathParameters'])
+        except Exception:
+            import traceback
+            log.error(self)
+            log.error(traceback.format_exc())
+            return response(body='Internal server Error', status=500)
+        if isinstance(resp, Response):
+            resp = resp.render()
+        return resp
+
+    def __str__(self):
+        return "<Request: {%s} %s (%r)" % (
+            self.request.method,
+            self.request.path,
+            self.requeste.params,
+        )
 
     def invalid(self, *args):
+        # XXX Build list of valid methods?
         return response(status=405)
