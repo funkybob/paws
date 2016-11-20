fd6702fbb43eb4e6c5129ac6026908946f03c1a7
Curtis Maloney <curtis@tinbrain.net>
2016-11-21 06:44:46 +1100

    Move request construction to init

--- paws/handler.py
+++ paws/handler.py
@@ -6,11 +6,12 @@ class Handler(object):
     '''
     Simple dispatcher class.
     '''
+    def __init__(self, event, context):
+        self.request = Request(event, context)
 
     def __call__(self, event, context):
-        self.request = request = Request(event, context)
         func = getattr(self, self.event['httpMethod'], self.invalid)
-        return func(request, *self.event['pathParameters'])
+        return func(self.request, *self.event['pathParameters'])
 
     def invalid(self, *args):
         return response(status=405)
