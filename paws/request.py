ec7bbe8ac8715ea22142680f0d880a7d0b71c687
Curtis Maloney <curtis@tinbrain.net>
2016-11-23 18:45:56 +1100

    Add cookies property to Request

--- paws/request.py
+++ paws/request.py
@@ -1,6 +1,7 @@
+from Cookie import SimpleCookie
 from urlparse import parse_qs
 
-from utils import cached_property, MultiDict
+from utils import MultiDict, cached_property
 
 
 class Request(object):
@@ -19,6 +20,13 @@ class Request(object):
     @cached_property
     def post(self):
         return MultiDict(parse_qs(self.event.get('body', '') or ''))
+
+    @cached_property
+    def cookies(self):
+        jar = SimpleCookie()
+        if self.event['headers'].get('Cookie'):
+            jar.load(self.event['headers']['Cookie'].encode('utf-8'))
+        return jar
 
     @property
     def stage(self):
