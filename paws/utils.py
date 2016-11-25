31533393d22e2f7e10031975fce4fbfa8c5d61b6
Curtis Maloney <curtis@tinbrain.net>
2016-11-25 17:25:22 +1100

    Ditch HeaderDict for now

--- paws/utils.py
+++ paws/utils.py
@@ -30,25 +30,3 @@ class cached_property(object):
             return self
         value = instance.__dict__[self.func.__name__] = self.func(instance)
         return value
-
-
-class HeaderDict(dict):
-    '''
-    Dict for containing response headers.
-
-    Keys are matched case insensitive.
-    title_items() will title-case keys.
-
-    XXX Non-destructive set?
-    '''
-    def __getitem__(self, key):
-        return super(HeaderDict, self).__getitem__(key.lower())
-
-    def __setitem__(self, key, value):
-        return super(HeaderDict, self).__setitem__(key.lower(), value)
-
-    def __delitem__(self, key):
-        return super(HeaderDict, self).__delitem__(key.lower())
-
-    def title_items(self):
-        return ((key.title(), value) for key, value in self.items())
