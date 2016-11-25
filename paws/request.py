8fa0fb5a36d191ff8e56268c662818fb195f6be9
Curtis Maloney <curtis@tinbrain.net>
2016-11-25 17:01:36 +1100

    Add Request.absolute_url.  Sorted methods/properties

--- paws/request.py
+++ paws/request.py
@@ -1,5 +1,5 @@
 from Cookie import SimpleCookie
-from urlparse import parse_qs
+from urlparse import parse_qs, urlunsplit
 
 from utils import MultiDict, cached_property
 
@@ -9,24 +9,32 @@ class Request(object):
         self.event = event
         self.context = context
 
-    @property
-    def method(self):
-        return self.event['httpMethod']
-
-    @property
-    def query(self):
-        return self.event['queryStringParameters']
-
-    @cached_property
-    def form(self):
-        return MultiDict(parse_qs(self.event.get('body', '') or ''))
-
     @cached_property
     def cookies(self):
         jar = SimpleCookie()
         if self.event['headers'].get('Cookie'):
             jar.load(self.event['headers']['Cookie'].encode('utf-8'))
         return jar
+
+    @cached_property
+    def form(self):
+        return MultiDict(parse_qs(self.event.get('body', '') or ''))
+
+    @property
+    def headers(self):
+        return self.event['headers']
+
+    @property
+    def method(self):
+        return self.event['httpMethod']
+
+    @property
+    def params(self):
+        return self.event['pathParameters']
+
+    @property
+    def query(self):
+        return self.event['queryStringParameters']
 
     @property
     def stage(self):
@@ -36,6 +44,7 @@ class Request(object):
     def stageVar(self):
         return self.event['stageVariables']
 
-    @property
-    def params(self):
-        return self.event['pathParameters']
+    def absolute_url(self, path):
+        return urlunsplit(
+            ('https', self.headers['Host'], '/'.join([self.stage, path]), '', ''),
+        )
