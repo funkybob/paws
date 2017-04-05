1e7777d19183a5e2453050bc29f1362c891d3496
Curtis Maloney <curtis@tinbrain.net>
2017-04-05 20:36:44 +1000

    Add way to annotate serverless config on a http handler

--- paws/handler.py
+++ paws/handler.py
@@ -9,6 +9,8 @@ log = logging.getLogger()
 class http_handler(object):
     def __init__(self, func):
         self.func = func
+        self.url = None
+        self.__name__ = func.__name__
 
     def __str__(self):
         return 'HTTP %s' % (self.func.__name__,)
@@ -26,3 +28,21 @@ class http_handler(object):
         if isinstance(resp, Response):
             resp = resp.render()
         return resp
+
+
+class url(object):
+    '''
+    @url(path[, cores=False][, method='GET'])
+    def http_function(request, ...):
+
+    http_function.url.path == path
+    '''
+    def __init__(self, path, cors=False, method='GET'):
+        self.path = path
+        self.cors = cors
+        self.method = method
+
+    def __call__(self, func):
+        func = http_handler(func)
+        func.url = self
+        return func
