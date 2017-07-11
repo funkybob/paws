f0199ab6a5d896446382bcbb1d96e964994c0dd6
Curtis Maloney <curtis@tinbrain.net>
2017-07-12 09:46:05 +1000

    Start test helpers

---
+++ paws/test.py
@@ -0,0 +1,29 @@
+from http.cookies import SimpleCookie
+from urllib.parse import urlunsplit
+
+from .utils import HeaderDict
+
+
+
+class MockRequest:
+    def __init__(self, method, url, *, cookies=None, headers=None, body=None):
+        self.method = method.upper()
+        self.cookies = SimpleCookie()
+        self.body = body if body is not None else b''
+        self.headers = HeaderDict()
+        if headers is not None:
+            self.headers.update(headers)
+        if cookies:
+            for name, value in cookies.items():
+                cookies[name] = value
+        # form
+        # params
+        # query
+        # stage
+        # stageVar
+
+    def absolute_url(self, path):
+        return urlunsplit(
+            ('https', self.headers['Host'], '/'.join([self.stage, path]), '', ''),
+        )
+
