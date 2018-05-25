b0e373626e10036716b1090ea183665087bb3b86
Curtis Maloney <curtis@tinbrain.net>
2018-05-26 00:29:36 +1000

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='PAWS',
-    version='0.0.1',
+    version='0.1.0',
     description='Python AWS Tools for Serverless',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
