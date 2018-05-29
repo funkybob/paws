428ce0c6d1d90eea1fb6e5fea192b92f2cd4ea36
Curtis Maloney <curtis@tinbrain.net>
2018-05-29 22:49:31 +1000

    Include readme as long description

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
+with open('README.md') as fin:
+    readme = fin.read()
 
 setup(
     name='PAWS',
     version='0.1.0',
     description='Python AWS Tools for Serverless',
+    long_description=readme,
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
     url='https://github.com/funkybob/paws',
